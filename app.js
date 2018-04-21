f55df237c2f447bb4838c68908c94b6e9cf6c0a2
ungear <redeemer@inbox.ru>
2018-04-21 22:04:39 +0300

    write to firebase

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const firebase = require("firebase");
 
 firebase.initializeApp(fbConfig);
 const database = firebase.database();
+
+// write
+let newRecordData = {
+  authorId: 1234,
+  text: new Date().getTime(),
+}
+let newKey = database.ref('wallRecords').push().key;
+let updates = {};
+updates['/wallRecords/' + newKey] = newRecordData;
+database.ref().update(updates);
+
+//read
 database.ref('wallRecords').once('value').then(snapshot => {
   console.log(snapshot.val())
   process.exit()
