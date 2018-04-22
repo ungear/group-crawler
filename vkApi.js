ada027e48272b47506fd905c3d80ea618de62b73
ungear <redeemer@inbox.ru>
2018-04-22 22:11:48 +0300

    reject on error

--- vkApi.js
+++ vkApi.js
@@ -3,16 +3,17 @@ const https = require('https');
 
 const groupId = vkConfig.vkGroupId;
 const accessToken = vkConfig.serviceKey;
+const apiVersion = '5.74';
 const vkApiRoot = 'https://api.vk.com/method';
 
 exports.getWallRecords = function ({offset = 0, count = 100} = {}) {
-  let url = `${vkApiRoot}/wall.get?owner_id=${groupId}&count=${count}&offset=${offset}&v=5.62&&access_token=${accessToken}`;
+  let url = `${vkApiRoot}/wall.get?owner_id=${groupId}&count=${count}&offset=${offset}&v=${apiVersion}&access_token=${accessToken}`;
 
   return new Promise((resolve, reject) => {
     https.get(url, res => {
       let responseData = '';
       res.on('data', chunk => responseData += chunk);
       res.on('end', () => resolve(JSON.parse(responseData).response))
-    })
+    }).on('error', e => reject(e))
   })
 }
