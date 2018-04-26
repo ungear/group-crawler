2bb852e678ac4a5d07d7f6885531f65d5e994768
Mikhail Istomin <mikhail_istomin@epam.com>
2018-04-26 21:07:07 +0300

    api reposts method

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/api/getTopRecordsByLikes', async function (req, res) {
   res.setHeader("Content-Type", "application/json")
   res.send(JSON.stringify(data))
 })
+app.get('/api/getTopRecordsByReposts', async function (req, res) {
+  let data = await dbService.getTopWallRecordsByReposts();
+  res.setHeader("Content-Type", "application/json")
+  res.send(JSON.stringify(data))
+})
 
 const port = 3008;
 app.listen(port, () =>{ console.log("Started at port " + port)})
