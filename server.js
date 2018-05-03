a6fddce912d89573fb3f3f934dc85b3f549deb1f
Mikhail Istomin <mikhail_istomin@epam.com>
2018-05-03 15:46:42 +0300

    allow CORS requests

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express()
 
 app.get('/api/getTopRecordsByLikes', async function (req, res) {
   let data = await dbService.getTopWallRecordsByLikes();
-  res.setHeader("Content-Type", "application/json")
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(JSON.stringify(data))
 })
 app.get('/api/getTopRecordsByReposts', async function (req, res) {
   let data = await dbService.getTopWallRecordsByReposts();
-  res.setHeader("Content-Type", "application/json")
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(JSON.stringify(data))
 })
 
